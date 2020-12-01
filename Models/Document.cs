6999249b4de1a7801f6321d6f380a071cbf9d19b
gerihal <gerahernan.al@gmail.com>
2020-12-01 14:19:20 +0100

    change branch to dev and added enums for Document Type

--- Models/Document.cs
+++ Models/Document.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DocumentsAPI.Models.enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,17 @@ namespace DocumentsAPI.Models
 {
     public class Document
     {
+        [Key]
         public int DocumentId { get; set; }
+
+        public DocumentType DocumentType { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string DocumentTitle { get; set; }
         public DateTime CreationDate  { get; set; }
+
+        [StringLength ( 600 )]
         public string DocumentText { get; set; }
         public string DocumentImage { get; set; }
     }
