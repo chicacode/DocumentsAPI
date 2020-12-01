9229862c3ba8392dae340d6769641d99df3e65a0
gerihal <gerahernan.al@gmail.com>
2020-12-01 14:05:50 +0100

    Add project files.

---
+++ Models/Document.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentsAPI.Models
+{
+    public class Document
+    {
+        public int DocumentId { get; set; }
+        public string DocumentTitle { get; set; }
+        public DateTime CreationDate  { get; set; }
+        public string DocumentText { get; set; }
+        public string DocumentImage { get; set; }
+    }
+}
---
+++ Models/DocumentType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentsAPI.Models
+{
+    public class DocumentType
+    {
+        public int DocumentTypeId { get; set; }
+
+        public string DocumentTypeName { get; set; }
+    }
+}
